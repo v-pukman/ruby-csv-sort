1de3a18e11732d8870adc4a3125b776ed3115da7
Victor Pukman <pukman.victor@gmail.com>
2025-10-23 17:02:22 +0300

    add heap

---
+++ lib/heap.rb
@@ -0,0 +1,68 @@
+class Heap
+  def initialize(&compare_block)
+    @heap = []
+    @compare_block = if block_given?
+      compare_block
+    else
+      lambda{|a, b| a > b }
+    end
+  end
+
+  def push(item)
+    @heap << item
+    rebalance_up(size - 1)
+    self
+  end
+
+  alias :<< :push
+
+  def pop
+    return nil if size == 0
+
+    swap(0, size - 1)
+    item = @heap.pop
+    rebalance_down(0)
+    item
+  end
+
+  def size
+    @heap.size
+  end
+
+  def peak
+    @heap[0]
+  end
+
+  private
+
+  def rebalance_up(index)
+    return if index == 0
+
+    parent = (index - 1) / 2
+    return unless compare(index, parent)
+
+    swap(index, parent)
+    rebalance_up(parent)
+  end
+
+  def rebalance_down(index)
+    left = 2 * index + 1
+    right = 2 * index + 2
+
+    if left < size && compare(left, index) && (right >= size || compare(left, right))
+      swap(index, left)
+      rebalance_down(left)
+    elsif right < size && compare(right, index)
+      swap(index, right)
+      rebalance_down(right)
+    end
+  end
+
+  def swap(a, b)
+    @heap[a], @heap[b] = @heap[b], @heap[a]
+  end
+
+  def compare(a, b)
+    @compare_block.call(@heap[a], @heap[b])
+  end
+end
---
+++ spec/heap_spec.rb
@@ -0,0 +1,83 @@
+require 'heap'
+
+RSpec.describe Heap do
+  let(:heap) { Heap.new }
+
+  describe "#push" do
+    it "adds item to heap storage" do
+      heap.push(5)
+      expect(heap.size).to eq 1
+    end
+
+    it "has an alias method" do
+      heap << 5 << 10
+      expect(heap.size).to eq 2
+    end
+
+    it "rebalances the heap" do
+      heap << 5
+      heap << 1
+      heap << 10
+      heap << 2
+      expect(heap.peak).to eq 10
+    end
+  end
+
+  describe "#pop" do
+    it "removes item from heap storage" do
+      heap << 10
+      heap << 1
+      expect(heap.size).to eq 2
+      heap.pop
+      expect(heap.size).to eq 1
+    end
+
+    it "returns max item" do
+      heap << 5
+      heap << 2
+      heap << 1
+      heap << 10
+      expect(heap.pop).to eq 10
+      expect(heap.pop).to eq 5
+      expect(heap.pop).to eq 2
+      expect(heap.pop).to eq 1
+    end
+  end
+
+  describe "#size" do
+    it "returns size of the heap" do
+      expect(heap.size).to eq 0
+      heap << 5
+      expect(heap.size).to eq 1
+    end
+  end
+
+  describe "#peak" do
+    it "returns max item" do
+      heap << 1
+      heap << 3
+      heap << 2
+      heap << 1
+      expect(heap.peak).to eq 3
+    end
+  end
+
+  context "when custom compare block is passed:" do
+    let(:heap) do
+      Heap.new do |a, b|
+        a[:amount] < b[:amount]
+      end
+    end
+    let(:user500) { {name: 'user500', amount: 500} }
+    let(:user100) { {name: 'user100', amount: 100} }
+    let(:user300) { {name: 'user300', amount: 300} }
+    it "uses custom block to compare" do
+      heap << user500
+      heap << user100
+      heap << user300
+      expect(heap.pop).to eq user100
+      expect(heap.pop).to eq user300
+      expect(heap.pop).to eq user500
+    end
+  end
+end
